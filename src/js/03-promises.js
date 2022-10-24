21f3888ec56921c99e3dd110aa97c461ba4b8163
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-10-24 18:09:51 +0300

    Create 03-promises.js

---
+++ src/js/03-promises.js
@@ -0,0 +1,13 @@
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
+
+// // ----- version 1 -----
+// // ----- version 2 -----
+// // ----- version 3 -----
+// // ----- version 4 -----
